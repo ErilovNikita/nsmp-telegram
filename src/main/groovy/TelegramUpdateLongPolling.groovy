843b6fc6449cd40fa6c3e15b66fcee449f42247b
Ерилов Никита <nerilov@octopeople.net>
2026-07-02 15:47:10 +0300

    Update TelegramUpdateLongPolling

--- src/main/groovy/TelegramUpdateLongPolling.groovy
+++ src/main/groovy/TelegramUpdateLongPolling.groovy
@@ -23,17 +23,26 @@ webHookSchedulerStatus?.trigger?.collect{ it.code }?.each{ api.scheduler.disable
 // Удаление информации о созданном веб-хуке
 telegram.Webhook.delete()
 
-// Получение и обработка обновлений
 Integer offset = api.keyValue.get('telegram', 'offset') ?: 0
-telegram.getUpdates(offset, 100, 30, [
-        UpdateType.CALLBACK_QUERY.value,
-        UpdateType.MESSAGE.value,
-        UpdateType.POLL.value,
-        UpdateType.POLL_ANSWER.value,
-        UpdateType.MESSAGE_REACTION.value
-]).each { update ->
-    processor.processUpdate(update)
-    offset = update.updateId + 1
-    api.keyValue.put('telegram', 'offset', value)
-}
+Boolean longPollingHasLaunched = api.keyValue.get('telegram', 'longPollingHasLaunched') as Boolean ?: false
 
+if (!longPollingHasLaunched) {
+    api.keyValue.put('telegram', 'longPollingHasLaunched', true)
+
+    // Получение и обработка обновлений
+    telegram.getUpdates(offset, 100, 30, [
+            UpdateType.CALLBACK_QUERY.value,
+            UpdateType.MESSAGE.value,
+            UpdateType.POLL.value,
+            UpdateType.POLL_ANSWER.value,
+            UpdateType.MESSAGE_REACTION.value
+    ]).each { update ->
+        processor.processUpdate(update)
+        offset = update.updateId + 1
+        api.keyValue.put('telegram', 'offset', value)
+    }
+
+    api.keyValue.put('telegram', 'longPollingHasLaunched', false)
+} else {
+    // просто с получения данных уже был запущен ранее
+}
